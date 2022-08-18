48ac31486b38ff09f580f6cb56d062443342ee2d
Qi Xi <xiqi5703@gmail.com>
2022-08-18 15:38:12 +0800

    use bootstrap to calculate confidence interval

--- scripts/cal_diff.py
+++ scripts/cal_diff.py
@@ -10,7 +10,7 @@ import json
 from unittest import result
 import matplotlib.pyplot as plt
 import numpy as np
-
+import statistics
 
 def moving_average(times, num_runs):
     times_padded = np.pad(
@@ -18,6 +18,51 @@ def moving_average(times, num_runs):
     )
     kernel = np.ones(num_runs) / num_runs
     return np.convolve(times_padded, kernel, mode="valid")
+
+
+def median(x):
+    x = sorted(x)
+    length = len(x)
+    mid, rem = divmod(length, 2)    # divmod函数返回商和余数
+    if rem:
+        return x[:mid], x[mid+1:], x[mid]
+    else:
+        return x[:mid], x[mid:], (x[mid-1]+x[mid])/2
+ 
+
+import numpy as np
+
+
+def average(data):
+    return sum(data) / len(data)
+
+
+def bootstrap(data, B, c, func):
+    """
+    计算bootstrap置信区间
+    :param data: array 保存样本数据
+    :param B: 抽样次数 通常B>=1000
+    :param c: 置信水平
+    :param func: 样本估计量
+    :return: bootstrap置信区间上下限
+    """
+    array = np.array(data)
+    n = len(array)
+    sample_result_arr = []
+    for i in range(B):
+        index_arr = np.random.randint(0, n, size=n)
+        data_sample = array[index_arr]
+        sample_result = func(data_sample)
+        sample_result_arr.append(sample_result)
+
+    a = 1 - c
+    k1 = int(B * a / 2)
+    k2 = int(B * (1 - a / 2))
+    auc_sample_arr_sorted = sorted(sample_result_arr)
+    lower = auc_sample_arr_sorted[k1]
+    higher = auc_sample_arr_sorted[k2]
+
+    return lower, higher
 
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -33,24 +78,41 @@ with open(args.file1) as f:
     results = json.load(f)["results"]
 
 mean1 = results[0]["mean"]
-# label = results[0]["command"]
-# times = results[0]["times"]
-# num = len(times)
-# nums = range(num)
+
+arr1 = results[0]["times"]
+arr1.sort()
+lHalf, rHalf, q2 = median(arr1)
 
 with open(args.file2) as f:
     results = json.load(f)["results"]
 mean2 = results[0]["mean"]
+arr2 = results[0]["times"]
+arr2.sort()
+
+arr = [arr2[i]-arr1[i] for i in range(0,len(arr1))]
+
+
+
+lHalf, rHalf, q2 = median(arr)
+print("*****medium*******")
+print(median(lHalf)[2])
+print(q2)
+print(median(rHalf)[2])
+print("******bootstrap******")
+
+result = bootstrap(arr, 1000, 0.95, average)
+print(result)
 
 # label = results[0]["command"]
 # times = results[0]["times"]
 # num = len(times)
 # nums = range(num)
 
-diff =mean1-mean2
+diff =mean2-mean1
 # fi = open('output.txt', 'w')
 
-print(args.title, "difference:", diff)
+print("difference:", diff, statistics.mean(arr))
 print('percent:{:.2%}'.format(diff/mean1))
+
 # print 'difference', diff
 # fi.close()
